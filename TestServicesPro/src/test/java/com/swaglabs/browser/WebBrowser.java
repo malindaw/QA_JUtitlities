faad1ae42fd520059beba3a310e31b392af46ebf
malinda <malinda@malindas-air>
2024-05-09 19:45:54 +0530

    First Commite

---
+++ TestServicesPro/src/test/java/com/swaglabs/browser/WebBrowser.java
@@ -0,0 +1,18 @@
+package com.swaglabs.browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebBrowser {
+	
+	static WebDriver driver;
+	
+	public void setupBrowser() {
+		
+		driver = new ChromeDriver();
+		
+		
+		
+	}
+
+}
